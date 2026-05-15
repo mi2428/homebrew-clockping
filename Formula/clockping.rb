6da264c5953e5cebcb349cdc5dc742f20e87d7d3
mi2428 <mi2428782020@gmail.com>
2026-05-15 13:12:10 +0900

    clockping 1.0.0

--- Formula/clockping.rb
+++ Formula/clockping.rb
@@ -10,22 +10,21 @@ class Clockping < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/mi2428/clockping/releases/download/v1.0.0/clockping-darwin-arm64",
+      url "https://github.com/mi2428/clockping/releases/download/v1.0.0/clockping-v1.0.0-darwin-arm64",
           using: :nounzip
-      sha256 "ff8e3d5f84867a62b7724623162cfb49450a22e879bf6f01c90cad03090ed06f"
+      sha256 "b99b58946bc8e98ef531c69588e4e55a9cdfcc02de951cbb361efc7f438249a9"
     end
 
     on_intel do
-      url "https://github.com/mi2428/clockping/releases/download/v1.0.0/clockping-darwin-amd64",
+      url "https://github.com/mi2428/clockping/releases/download/v1.0.0/clockping-v1.0.0-darwin-amd64",
           using: :nounzip
-      sha256 "bb99371a2c1a8ceb2814370b7c7acd4beb071e3a2ad1faf818e9ea9bd6bbba82"
+      sha256 "77ae2e870a224f0801e5e57e30602271bc089fd048694a61b7d12c4f4d5a36e2"
     end
   end
 
   def install
-    bin.install Dir["clockping-darwin-*"].first => "clockping"
+    bin.install Dir["clockping-v#{version}-darwin-*"].first => "clockping"
     chmod 0755, bin/"clockping"
-    generate_completions_from_executable(bin/"clockping", "completion")
   end
 
   test do
